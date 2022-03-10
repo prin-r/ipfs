a73e87b081354b252cfb8ea914c66df6d3e572ed
Jipyeongseon Geunyang Samgagbeob <prin-r@users.noreply.github.com>
2022-03-10 16:55:13 +0700

    Create get_and_return_simple_json.rs

---
+++ get_and_return_simple_json.rs
@@ -0,0 +1,26 @@
+use obi::{OBIDecode, OBIEncode, OBISchema};
+use owasm::{execute_entry_point, ext, oei, prepare_entry_point};
+
+const DS:i64 = 263;
+
+#[derive(OBIDecode, OBISchema)]
+struct Input {
+    sliced_index_input: i8
+}
+
+#[derive(OBIEncode, OBISchema)]
+struct Output {
+    result: String,
+}
+
+fn prepare_impl(input: Input) {
+    oei::ask_external_data(DS, 1, format!("{}", input.sliced_index_input).as_bytes())
+}
+
+#[no_mangle]
+fn execute_impl(_input: Input) -> Output {
+    Output { result: ext::load_majority::<String>(1).unwrap() }
+}
+
+prepare_entry_point!(prepare_impl);
+execute_entry_point!(execute_impl);
