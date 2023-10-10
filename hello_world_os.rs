5bf0f1db27bc68175c35ac867a1096a43fcc1def
Jipyeongseon Geunyang Samgagbeob <prin-r@users.noreply.github.com>
2023-10-11 04:31:37 +0700

    Create hello_world_os.rs

---
+++ hello_world_os.rs
@@ -0,0 +1,23 @@
+use obi::{OBIDecode, OBIEncode, OBISchema};
+use owasm_kit::{execute_entry_point, ext, oei, prepare_entry_point};
+
+#[derive(OBIDecode, OBISchema)]
+struct Input { word: String }
+
+#[derive(OBIEncode, OBISchema)]
+struct Output { result: String }
+
+fn prepare_impl(input: Input) {
+    if input.word.len() > 0 {
+        panic!("Error: word.len() must be > 0");
+    }
+
+    oei::ask_external_data(6, 1, input.word.as_bytes());
+}
+
+fn execute_impl(_: Input) -> Output {
+    Output { result: ext::load_majority::<String>(1).unwrap() }
+}
+
+prepare_entry_point!(prepare_impl);
+execute_entry_point!(execute_impl);
